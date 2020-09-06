89936d1fdfceb60f5846c5b08789a3f07d571838
Soroush Khanlou <soroushkhanlou@gmail.com>
2020-09-06 12:15:48 -0400

    make a new method property wrapper

---
+++ Meridian/Property Wrappers/Method.swift
@@ -0,0 +1,16 @@
+//
+//  Method.swift
+//  
+//
+//  Created by Soroush Khanlou on 9/6/20.
+//
+
+import Foundation
+
+public typealias RequestMethod = Custom<HTTPMethodExtractor>
+
+public struct HTTPMethodExtractor: NonParameterizedExtractor {
+    public static func extract(from context: RequestContext) throws -> HTTPMethod {
+        return context.header.method
+    }
+}
