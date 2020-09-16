65a6f539ae32ccbbafadd61296c3552aa23f69aa
Soroush Khanlou <soroushkhanlou@gmail.com>
2020-09-15 23:21:04 -0400

    add a test for http method property wrapper

---
+++ MeridianTests/HTTPMethodRouteTests.swift
@@ -0,0 +1,43 @@
+//
+//  HTTPMethodRouteTests.swift
+//  
+//
+//  Created by Soroush Khanlou on 9/15/20.
+//
+
+import XCTest
+import NIO
+import NIOHTTP1
+@testable import Meridian
+
+struct HTTPMethodTestRoute: Responder {
+
+    @RequestMethod var method
+
+    func execute() throws -> Response {
+        "The method was \(method)"
+    }
+}
+
+final class HTTPMethodRouteTests: XCTestCase {
+
+    func makeWorld() throws -> World {
+        return try World(routes: [
+            HTTPMethodTestRoute()
+                .on("/method"),
+        ])
+    }
+
+    func testRandomly() throws {
+
+        let world = try self.makeWorld()
+
+        let method = Meridian.HTTPMethod.primaryMethods.randomElement()!
+
+        try world.send(HTTPRequestBuilder(uri: "/method", method: NIOHTTP1.HTTPMethod(rawValue: method.name)))
+
+        let response = try world.receive()
+        XCTAssertEqual(response.statusCode, .ok)
+        XCTAssertEqual(response.bodyString, "The method was \(method)")
+    }
+}
