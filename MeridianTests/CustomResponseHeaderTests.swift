5d5be47498a1b8f651270718a0cb2c6f8e84d224
Soroush Khanlou <soroushkhanlou@gmail.com>
2021-02-03 22:09:12 -0500

    ensure that the last header wins

---
+++ MeridianTests/CustomResponseHeaderTests.swift
@@ -0,0 +1,41 @@
+//
+//  CustomStatusCodeTests.swift
+//  
+//
+//  Created by Soroush Khanlou on 9/3/20.
+//
+
+import XCTest
+import NIO
+import NIOHTTP1
+@testable import Meridian
+
+struct CustomResponseHeaderTestRoute: Responder {
+    func execute() throws -> Response {
+        "Hello"
+            .additionalHeaders(["X-Custom-Header": "testing"])
+            .additionalHeaders(["X-Custom-Header": "testing2"])
+    }
+}
+
+final class CustomResponseHeaderTests: XCTestCase {
+    
+    func makeWorld() throws -> World {
+        return try World(routes: [
+            CustomResponseHeaderTestRoute()
+                .on("/customHeader"),
+        ])
+    }
+    
+    func testBasic() throws {
+        
+        let world = try self.makeWorld()
+        
+        try world.send(HTTPRequestBuilder(uri: "/customHeader", method: .GET))
+
+        let response = try world.receive()
+
+        XCTAssertEqual(response.headers.first(where: { name, value in name == "X-Custom-Header"})?.value, "testing2", "The last header in the stack should win.")
+        XCTAssertEqual(response.bodyString, "Hello")
+    }
+}
