a4a6637ddb4ebbcd2e519ab0ce373ed813d2f8eb
Soroush Khanlou <soroushkhanlou@gmail.com>
2023-03-11 13:36:11 -0500

    publicize the loop group

--- Meridian/EnvironmentValues.swift
+++ Meridian/EnvironmentValues.swift
@@ -43,7 +43,7 @@ struct LoopGroupEnvironmentKey: EnvironmentKey {
 }
 
 extension EnvironmentValues {
-    var loopGroup: EventLoopGroup {
+    public var loopGroup: EventLoopGroup {
         get {
             self[LoopGroupEnvironmentKey.self]
         }
